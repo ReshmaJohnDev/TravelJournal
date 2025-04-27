c16c5bf9b66440f9dfd75620c2b5ee7a201aecbd
Reshma John <reshma.chackola@gmail.com>
2025-04-27 06:55:00 +0200

    First React project

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import Header from "./components/Header";
+import Entry from "./components/Entry";
+import journalData from "./data.js";
+
+/**
+ * Challenge:
+ * - import the array of data from data.js
+ * - map over the array to create an <Entry /> component
+ *   for every item in the data array.
+ * - display the array of Entry components in place of the current
+ *   hard-coded <Entry /> instance.
+ */
+
+const journalElements = journalData.map((journal) => {
+  return (
+    <Entry
+      img={journal.img}
+      title={journal.title}
+      country={journal.country}
+      googleMapsLink={journal.googleMapsLink}
+      dates={journal.dates}
+      text={journal.text}
+    />
+  );
+});
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main className="container">{journalElements}</main>
+    </>
+  );
+}
